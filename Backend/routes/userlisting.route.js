f40ea0dbdcc80e0d2f35e2af49865634f54a264d
Manvendrashih <vishvajitherma@gmail.com>
2025-04-07 18:17:20 +0530

    change userlisting Controller name

--- Backend/routes/userlisting.route.js
+++ Backend/routes/userlisting.route.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const router = express.Router();
-const { userlisting } = require('../controllers/userlisting');
+const { userlisting } = require('../controllers/userlisting.controllers');
 
 // Create a route to handle form submission
 router.post('/api/properties', userlisting);
